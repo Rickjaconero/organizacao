e526a4fae7889fcdcc60c44ceb767041c400255a
rickjaconero <rfa.guimaraes@hotmail.com>
2019-10-15 20:42:32 -0300

    Mudança de Linguage,

---
+++ Organizacao/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Reflection;
+using System.IO;
+using Tweetinvi;
+using Autofac;
+using Newtonsoft.Json;
+using Nito.AsyncEx;
+using Tweetinvi.Parameters;
+using Tweetinvi.Models;
+using Newtonsoft.Json.Linq;
+using Organizacao;
+using Nancy.Json;
+using Nancy.Json.Simple;
+using RestSharp;
+using Tweetinvi.Core.Events;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private TweetRetorno t;
+       
+
+        static void Main(string[] args)
+        {
+            Auth.SetUserCredentials("TXlcjpRTbZEgmkoxNT2oqCfeX", "C5BzmoaPbLSoZ8De8qej7aSb0OCz0f2MlouLybtDdY5freEhNN", "97486111-ySo0byQk5B1KXHRMSNMTUABulYn4VRb1nC0aCNHgS", "ixkpMxe6dMMOhL4lKF5yFVgWbnleB8KtO0tJruqkV0EbI");
+            var authUser = User.GetAuthenticatedUser();
+            var settings = authUser.GetAccountSettings();
+            var matchingTweets = Search.SearchTweets("Libertadores").ToJson();
+            var lista = matchingTweets[1];
+            Console.WriteLine(lista);
+
+            var test = String.Empty;
+
+            //inserir no fim do arquivo
+            if (File.Exists(@"Dados.json"))
+            {
+                test = File.ReadAllText(@"Dados.json");
+            }
+            File.WriteAllText(@"Dados.json",test + Environment.NewLine + matchingTweets);
+            Console.WriteLine("inseriu no fim do arquivo");    
+
+            //metodo 3 
+            JArray a = JArray.Parse(matchingTweets);
+            var c = a.Count;
+            var i = 0;
+            while (c > 0)
+            {
+                //ta pulando de 2 em 2
+                Console.WriteLine("O valor de c eh = " + c.ToString());
+                Console.WriteLine("O valor de i eh = " + i.ToString());
+
+                Console.ReadLine();//pause
+
+ 
+                Console.WriteLine(a[i]["id"].ToString());
+
+                //Console.WriteLine(a.);
+                c--;
+                i++;
+            }
+           
+            Console.WriteLine(a.GetType());
+            
+
+
+
+
+
+        }
+
+
+
+    }
+}
---
+++ Organizacao/TweetRetorno.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Organizacao
+{
+    class TweetRetorno
+    {
+        public int id_tweet { get; set; }
+
+        public String message { get; set; }
+
+        public int user_id{ get; set; }
+
+        public String user { get; set; }
+
+        public TweetRetorno()
+        {
+
+        }
+
+        
+    }
+}
